e22abbdcc0b0aaa5ac1ae7fbc4862ba2772a3653
Adriano Miranda <adriano.miranda.ti@gmail.com>
2021-07-21 12:36:48 -0300

    tetando a primeira classe de domínio - Categoria

--- src/main/java/com/adriano/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/adriano/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,37 @@
+/*Pacote de recusos em inglês
+ * é um padrão aonde vamos gravar as nossas classes que são controladoras REST
+ * São chamadas de Resourses (recursos) 
+ * Como nosso desenvolvimento ele é orientado a domínio, classes de domínio, sempre que for criar um recurso, sempre vamos usar
+ * como padrão o nome da classe de dominio mais o sufixo Resource   - CategoriaResource*/
+
 package com.adriano.cursomc.resources;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import domain.Categoria;
+
 @RestController
-@RequestMapping(value = "/categorias")
+@RequestMapping(value = "/categorias")/*Criando um um and point REST.*/ 
+/*Feito isto, criamos uma classe, que vai ser um controlador REST e vai responder por este and point /categorias*/
 public class CategoriaResource {
 
+	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest  está funcionando";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1,"Casa");
+		Categoria c2 = new Categoria(2,"Mesa");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		return lista;
 	}
+	
+	
+	
 }
---
+++ src/main/java/domain/Categoria.java
@@ -0,0 +1,80 @@
+package domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+
+public class Categoria implements Serializable {
+	/*Serializable é uma interface que diz que esta classe, os objetos dela poderá ser convertido para uma sequencia de bits, para que
+	 * os objetos sejam gravados em arquivos, trafegar em redes. Exigencia da linguagem Java */
+	
+	private static final long serialVersionUID = 1L; /*Quando uma classe implementa umSerializable, ele precisa de um número de 
+	versao padrao, neste caso a minha classe é a versão 1 dela, primeira versao */
+	
+	
+	private Integer id;
+	private String nome;
+	
+	
+	//***********************Consturores
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	
+	
+	//***********************GETS e SETS
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	
+	//***********************TO STRING
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nome=" + nome + "]";
+	}
+
+	
+	//***********************HASH CODE e EQUALS --- Compara os objetos por valor
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+	
+}
