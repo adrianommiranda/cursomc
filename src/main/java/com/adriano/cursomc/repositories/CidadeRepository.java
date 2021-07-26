c91b28a05d487a5eccf563b0e79c43358c679b02
Adriano Miranda <adriano.miranda.ti@gmail.com>
2021-07-26 14:10:38 -0300

    estado e cidade

---
+++ src/main/java/com/adriano/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,16 @@
+package com.adriano.cursomc.repositories;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adriano.cursomc.domain.Cidade;
+
+
+
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
